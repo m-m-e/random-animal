845933e78ee703dfc13a63e817d33e7f68a13468
Megan Edwards <megmyfedwards@gmail.com>
2019-05-08 11:01:04 +0200

    Add styles

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const buttons = document.querySelectorAll('.btn');
 const resultsBox = document.querySelector('.results');
 const urlKitty = 'https://api.thecatapi.com/v1/images/search';
+const urlDog = 'https://dog.ceo/api/breeds/image/random';
 
 const printPic = url => {
   resultsBox.innerHTML = '';
@@ -20,15 +21,31 @@ const printPic = url => {
   resultsBox.appendChild(pic);
 }
 
+const printAltPic = url => {
+  resultsBox.innerHTML = '';
+  const pic = document.createElement('img');
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const src = data.message;
+      pic.classList.add('image');
+      pic.setAttribute('src', src);
+      pic.setAttribute('height', '400px');
+    })
+
+  resultsBox.appendChild(pic);
+}
+
 const getSomething = event => {
   const targetId = parseInt(event.currentTarget.id); 
   if (targetId === 1) {
     printPic(urlKitty);
   } else if (targetId === 2) {
-    printPic(2);
-  } else {
-    printPic(3);
-  }
+    printAltPic(urlDog);
+  } 
+  // else {
+  //   printPic(3);
+  // }
 };
 
 for (const button of buttons) {
